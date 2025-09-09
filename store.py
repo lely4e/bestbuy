20bc964f0304a16e0e038fcc5350191a139feceb
Olga Chirkova <lely4e@gmail.com>
2025-09-09 17:49:50 +0200

    Main function with user interface added

--- store.py
+++ store.py
@@ -27,26 +27,10 @@ class Store:
 
     def order(self, shopping_list) -> float:
         """ Order the product in the store """
-        # Gets a list of tuples, where each tuple has 2 items:
-        # Product (Product class) and quantity (int)
+        # Gets a list of tuples, where each tuple has 2 items: Product (Product class) and quantity (int)
         total = 0
         for product, quantity in shopping_list:
             total += product.buy(quantity)
         return total
 
 
-def main():
-
-    product_list = [products.Product("MacBook Air M2", price=1450, quantity=100),
-                    products.Product("Bose QuietComfort Earbuds", price=250, quantity=500),
-                    products.Product("Google Pixel 7", price=500, quantity=250),
-                    ]
-
-    best_buy = Store(product_list)
-    product_items = best_buy.get_all_products()
-    print(best_buy.get_total_quantity())
-    print(best_buy.order([(product_items[0], 1), (product_items[1], 2)]))
-
-
-if __name__ == "__main__":
-    main()
